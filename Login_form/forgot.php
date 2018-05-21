97f25651d9b9fe3a6e352c78fe993ce9ab3a2eff
YK <38709555+ykkf@users.noreply.github.com>
2018-05-21 14:54:32 +0900

    ログインフォーム（パスワード問い合わせ未完成）

---
+++ Login_form/forgot.php
@@ -0,0 +1,17 @@
+<?php
+
+ ?>
+
+ <!doctype html>
+ <html>
+ <head>
+   <meta charset="UTF-8">
+ </head>
+ <title>パスワードを忘れた場合</title>
+ <body>
+  メールアドレスを入力してください
+  <br>
+   <input type="text">
+   <input type="submit">
+</body>
+ </html>
