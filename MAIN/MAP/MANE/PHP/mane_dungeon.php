9e698773a87a121b49256a8f8d9461b63afeeebf
gondayo <38709416+gondayo@users.noreply.github.com>
2018-10-11 00:21:30 +0900

    ファイル操作

---
+++ MAIN/MAP/MANE/PHP/mane_dungeon.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>tec_dungeon</title>
+</head>
+<link rel="stylesheet" type="text/css" href="MAP/CSS/mane_dungeon.css">
+<body>
+
+    <div class="mane_list">
+      <a href="game.html"class="mane_name_a">
+        ボタン
+      </a>
+    </div><br>
+
+</body>
+</html>
---
+++ MAIN/MAP/TEC/PHP/tec_dungeon.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>tec_dungeon</title>
+</head>
+<link rel="stylesheet" type="text/css" href="MAP/CSS/tec_dungeon.css">
+<body>
+
+    <div class="tec_list">
+      <a href="game.html"class="tec_name_a">
+        ボタン
+      </a>
+    </div><br>
+
+    <div class="tec_list">
+      <a href="game.html"class="tec_name_a">
+        ボタン
+      </a>
+    </div><br>
+
+    <div class="tec_list">
+      <a href="game.html"class="tec_name_a">
+        ボタン
+      </a>
+    </div><br>
+
+    <div class="tec_list">
+      <a href="game.html"class="tec_name_a">
+        ボタン
+      </a>
+    </div><br>
+
+    <div class="tec_list">
+      <a href="game.html"class="tec_name_a">
+        ボタン
+      </a>
+    </div><br>
+
+    <div class="tec_list">
+      <a href="game.html"class="tec_name_a">
+        ボタン
+      </a>
+    </div><br>
+</body>
+</html>
