c8bd4d019862d76c8cf78544d150bbe3d7ef2c2b
miftra <tsumirari63@docomo.ne.jp>
2018-10-31 10:13:07 +0900

    quiz

--- MAIN/ITEM/PHP/Item.php
+++ MAIN/ITEM/PHP/Item.php
@@ -9,6 +9,10 @@ define('DB_PASSWORD','g015c1316');
 // エラーメッセージの初期化
 $errorMessage = "";
 
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
 // ユーザのアイテム情報をすべて表示する
 try {
 $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>FALSE));
@@ -27,35 +31,51 @@ $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE=>PDO::ERRMO
 }
 
 // ユーザのアイテム使用処理
-if (isset($_POST["ok"])){
 
-  $ItemNum = $_POST["UseNum"];
-  $ItemId = $_POST["ok"];
+  if (isset($_POST["ok"])){
 
-  function check($ItemNum){
-    if($ItemNum === 0){
-      throw new Exception('アイテムの所持数がありません。');
+    //モーダルとクリックされたアイテム名の改ざん検知的なことをしたかった
+    if($_POST["ItemName"] === $_POST["data-name"]){
+      $ItemName = $_POST["ItemName"];
+
+      function check($ItemNum){
+        if($ItemNum === 0){
+          throw new Exception('アイテムの所持数がありません。');
+        }
+      }
+
+    try {
+      $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>FALSE));
+
+      //アイテム名とアイテムIDで指定した個数を取得
+      $stmta = $pdo->prepare('SELECT ItemNum FROM u_item WHERE ItemName = ? AND UserId = ?');
+      $stmta->bindvalue(1,$ItemName,PDO::PARAM_INT);
+      $stmta->bindvalue(2,(int)$UserId["UserId"],PDO::PARAM_INT);
+      $stmta->execute();
+      $ItemNum = $stmta->fetchAll(PDO::FETCH_COLUMN,0);
+      check($ItemNum);
+      $ItemNum = $ItemNum - 1;
+
+      //アイテム個数を減らす
+      $stmt = $pdo->prepare('UPDATE u_item SET ItemNum = ? WHERE ItemName = ? AND UserId = ?');
+      $stmt->bindvalue(1,(int)$ItemNum,PDO::PARAM_INT);
+      $stmt->bindvalue(2,$ItemName,PDO::PARAM_INT);
+      $stmt->bindvalue(3,(int)$UserId["UserId"],PDO::PARAM_INT);
+      $stmt->execute();
+      //この時点でアイテム使用フラグをu_itemに送りダンジョンクリア後にフラグ消去のほうが良い？
+
+      header("Location: Itemcheck.php");
+      exit();
+        } catch (Exception $e) {
+          $errorMessage = $e->getMessage();
+      }
+      // セッションの変数のクリア
+      $_SESSION = array();
+
+      // セッションクリア
+      @session_destroy();
     }
   }
-
-  try {
-    $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>FALSE));
-    check($ItemNum);
-    $ItemNum = --$ItemNum;
-
-    $stmt = $pdo->prepare('UPDATE u_item SET ItemNum = ? WHERE ItemId = ? AND UserId = ?');
-    $stmt->bindvalue(1,(int)$ItemNum,PDO::PARAM_INT);
-    $stmt->bindvalue(2,(int)$ItemId,PDO::PARAM_INT);
-    $stmt->bindvalue(3,(int)$UserId["UserId"],PDO::PARAM_INT);
-    $stmt->execute();
-    //この時点でアイテム使用フラグをu_itemに送りダンジョンクリア後にフラグ消去のほうが良い？
-
-    header("Location: Itemcheck.php");
-    exit();
-      } catch (Exception $e) {
-    $errorMessage = $e->getMessage();
-  }
-}
  ?>
 
 <!doctype html>
@@ -67,26 +87,27 @@ if (isset($_POST["ok"])){
    <script src="/MAIN/ITEM/JS/Item.js"></script>
  </head>
  <body>
-
+   <form method="POST">
    <ul>
 
    <?php
     foreach ($stmt as $Items){
       ?>
-    <li><span class="ItemName"><?php echo htmlspecialchars($Items["ItemName"], ENT_QUOTES, 'UTF-8');?></span>
-        <span class="ItemNum"><?php echo htmlspecialchars($Items["ItemNum"], ENT_QUOTES, 'UTF-8');?></span>
-        <button class="use" data-name="<?php echo htmlspecialchars($Items["ItemName"], ENT_QUOTES, 'UTF-8');?>" data-num="<?php echo htmlspecialchars($Items["ItemNum"], ENT_QUOTES, 'UTF-8');?>"value="<?php echo htmlspecialchars($Items["ItemId"], ENT_QUOTES, 'UTF-8');?>">使う</button>
+    <li><span class="ItemName"><?php echo h($Items["ItemName"]);?></span>
+        <span class="ItemNum"><?php echo h($Items["ItemNum"]);?></span><span>個</span>
+        <button type="submit" class="use" data-name="<?php echo h($Items["ItemName"]);?>" data-num="<?php echo h($Items["ItemNum"]);?>" value="<?php echo h($Items["ItemName"]);?>">使う</button>
     </li>
    <?php
     }
       ?>
 
-  </ul>
+    </ul>
+  </form>
   <form method="POST">
    <div id="modal-window">
      <span id="UseItem"></span><span class="check">を使用しますか？</span>
      <span id="UseNum" name="UseNum"></span><span class="check">個</span>
-   </div>
+     <input id="Name" type="hidden" name="ItemName" value="">
   </form>
  </body>
 </html>
